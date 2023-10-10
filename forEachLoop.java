fcf5a02cb28ed77956d496f6f3bd0b2237f7b867
pius11 <pius17431@gmail.com>
2023-10-10 09:25:30 +0800

    forEachLoop

---
+++ forEachLoop.java
@@ -0,0 +1,12 @@
+/**
+ * forEachLoop
+ */
+public class forEachLoop {
+
+    public static void main(String[] args) {
+        String[] cars = { "BMW", "TOYOTA", "TEZLA", "MAZDA", "VOLVO" };
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
